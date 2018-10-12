634c8588f0ceb5d2f3ff4c7bb1809dbf7dcfd343
Jason Beggs <jasonlbeggs@gmail.com>
2018-10-11 20:47:37 -0400

    Refactor and restyle

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app" class="text-grey-darkest">
-    <div class="container mx-auto px-4 py-16 md:flex flex-col items-center justify-center min-h-screen">
-      <h1 :class="{'opacity-0': disabled}" class="text-3xl md:text-4xl text-black mb-4 transition">{{ result }}</h1>
-      <button :disabled="disabled" class="mb-6 bg-blue text-white shadow-md rounded-lg px-4 py-3 text-xl uppercase tracking-wide font-medium focus:outline-none focus:shadow-outline" @click.prevent="generate">Choose</button>
-      <div class="w-full md:w-auto text-left">
-        <h2 class="text-black mb-3">Options:</h2>
-        <div v-if="options.length" class="mb-4">
-          <div v-for="option in options" class="flex items-center justify-between mb-2" :key="option">
-            <div class="text-xl text-grey-dark">{{ option }}</div>
-            <div
-              class="font-bold text-2xl text-grey cursor-pointer hover:text-red"
-              @click.prevent="removeOption(option)"
-            >
-              &times;
+  <div id="app">
+    <transition name="fade">
+      <div v-if="!result" class="container mx-auto px-4 py-16 md:flex flex-col items-center justify-center min-h-screen">
+        <h1 class="text-3xl md:text-4xl text-black">Random Picker</h1>
+        <h2 class="text-xl md:text-2xl text-grey-darker mb-6 font-medium">Click the button to choose a random option.</h2>
+        <button class="w-full md:w-auto mb-6 bg-teal hover:bg-teal-dark text-white shadow-md rounded-lg px-4 py-3 text-xl uppercase tracking-wide font-medium focus:outline-none focus:shadow-outline" @click.prevent="generate">Choose</button>
+        <div class="w-full md:w-auto text-left">
+          <h2 class="text-black mb-3">Options:</h2>
+          <div v-if="options.length" class="mb-4">
+            <div v-for="option in options" class="flex items-center justify-between mb-2" :key="option">
+              <div class="text-xl text-grey-dark">{{ option }}</div>
+              <button
+                class="font-bold text-2xl text-grey cursor-pointer hover:text-red"
+                @click.prevent="removeOption(option)"
+              >
+                &times;
+              </button>
             </div>
           </div>
+          <div v-else class="text-xl text-grey mb-4">Add an option to generate a random pick.</div>
+          <form class="flex items-center" @submit.prevent="addOption">
+            <input placeholder="Add an option" v-model="newOption" class="w-full bg-white px-4 py-3 text-xl rounded-lg shadow-md mr-1 focus:outline-none focus:shadow-outline">
+            <button class="bg-teal hover:bg-teal-dark text-white shadow-md rounded-lg px-4 py-3 text-xl uppercase tracking-wide font-medium focus:outline-none focus:shadow-outline">Add</button>
+          </form>
         </div>
-        <div v-else class="text-xl text-grey mb-4">Add an option to generate a random pick.</div>
-        <form class="flex items-center" @submit.prevent="addOption">
-          <input placeholder="Add an option" v-model="newOption" class="w-full bg-white px-3 py-2 rounded-lg shadow-md mr-1 focus:outline-none focus:shadow-outline">
-          <button class="bg-blue text-white shadow-md rounded-lg px-3 py-2 uppercase tracking-wide font-medium focus:outline-none focus:shadow-outline">Add</button>
-        </form>
       </div>
-    </div>
+      <h1 v-else class="text-teal text-center container px-4 py-16 flex items-center justify-center min-h-screen">
+        {{ result }}
+      </h1>
+    </transition>
   </div>
 </template>
 
@@ -34,7 +40,7 @@ export default {
       newOption: null,
       disabled: false,
       options: ["Watch more TV", "Go to sleep"],
-      result: "Click the button to choose a random option."
+      result: null
     };
   },
   mounted() {
@@ -68,23 +74,25 @@ export default {
         return;
       }
 
-      this.disabled = true;
-
-      const chosen = this.options[
+      this.result = this.options[
         Math.floor(Math.random() * this.options.length)
       ];
 
       setTimeout(() => {
-        this.result = `Result: ${chosen}`;
-        this.disabled = false;
-      }, 500);
+        this.result = null;
+      }, 3000);
     }
   }
 };
 </script>
 
 <style>
-.transition {
-  transition: all 0.2s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
